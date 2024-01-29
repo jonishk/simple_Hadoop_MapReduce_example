58793318de55d107628367082800df4da7e5d73b
jonishk <jonishk@gmail.com>
2024-01-29 00:12:40 +0000

    Update stopwords and punctuation handling

--- mapper.py
+++ mapper.py
@@ -12,9 +12,11 @@ for line in sys.stdin:
     # split the line into words; splits on any whitespace
     words = line.split()
 
-    # output tuples (word, 1) in tab-delimited format
-    stopwords = set(['the', 'and', 'to', 'a', 'for'])
+    # set stopwords and punctuations
+    stopwords = set(['the', 'and', '.', ',', ':', ';', '-', '\'', '\"', '?', '!'])
 
+    # output tuples (word, 1) in tab-delimited format
     for word in words:
         if word not in stopwords:
             print '%s\t%s' % (word, "1")
+
